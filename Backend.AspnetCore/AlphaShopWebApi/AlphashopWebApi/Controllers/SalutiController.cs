df5d96d9dbbe6de93e4e2566dec420e693214527
Asia Casella <acasella@edok.it>
2022-10-13 16:09:06 +0200

    be  2.0

---
+++ Backend.AspnetCore/AlphaShopWebApi/AlphashopWebApi/Controllers/SalutiController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AlphashopWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/saluti")]
+    public class SalutiController
+    {
+        public string getSaluti()
+        {
+            return "Sono la tua prima web api creata in c#";
+        }
+
+    }
+}
